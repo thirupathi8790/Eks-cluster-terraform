20e126a862123aeeaf8eccc84891a9518a793d50
thirupathi8790 <128212210+thirupathi8790@users.noreply.github.com>
2024-08-01 12:31:02 +0530

    Update main.tf

--- Eks-Cluster/main.tf
+++ Eks-Cluster/main.tf
@@ -2,40 +2,6 @@ provider "aws" {
   region = "us-east-1"  # Change this to your desired region
 }
 
-resource "aws_eks_cluster" "Eks" {
-  name     = "Eks-cluster"
-  role_arn  = aws_iam_role.eks.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.eks[*].id
-  }
-
-  # Specify EKS version
-  version = "1.24"  # Change to the version you need
-}
-
-resource "aws_iam_role" "eks" {
-  name = "Demo-eks-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks" {
-  role       = aws_iam_role.eks.name
-  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
 resource "aws_vpc" "Eks" {
   cidr_block = "10.0.0.0/16"
 
@@ -57,14 +23,35 @@ resource "aws_subnet" "Eks" {
 
 data "aws_availability_zones" "available" {}
 
-output "cluster_name" {
-  value = aws_eks_cluster.Eks.name
+resource "aws_iam_role" "eks" {
+  name = "Demo-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.Eks.endpoint
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  role       = aws_iam_role.eks.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-output "cluster_arn" {
-  value = aws_eks_cluster.Eks.arn
+resource "aws_eks_cluster" "Eks" {
+  name     = "Eks-cluster"
+  role_arn  = aws_iam_role.eks.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.Eks[*].id
+  }
+
+  version = "1.24"  # Change to the version you need
 }
